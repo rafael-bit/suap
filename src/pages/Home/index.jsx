141d4db730e9ffe5c41d99339eda3236fc3b7dab
rafael-bit <raquila743@gmail.com>
2023-03-04 10:27:21 -0300

    Terminando Funcionalidade do Header e Começando tabela

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,13 +9,13 @@ export default function Home() {
 			<Header />
 			<Content>
 				<h1>Olá, Rafael !</h1>
-				<Image src="/profile.jpg" width="200" height="200"></Image>
+				<Image src="/profile.jpg" width="200" height="200" />
 				<ul class="tabs" role="tablist">
 					<li>
 						<input type="radio" name="tabs" id="tab1" />
 						<label for="tab1" role="tab" aria-selected="true" aria-controls="panel1" tabindex="0">Dados pessoais</label>
 						<div id="tab-content1" class="tab-content" role="tabpanel"	aria-labelledby="description" aria-hidden="false">
-							<p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 						</div>
 						</li>
 						<li>
